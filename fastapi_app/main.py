8bd5473c32b693ff3000eb8750a9ca7fcf030e47
agustinmend <jose.mendoza.u1@ucb.edu.bo>
2026-05-29 07:23:22 -0400

    Agregando readme

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -3,7 +3,12 @@ from api.v1.routers import router as api_router
 from repositories.session_repo import SessionRepositoryProtocol, PostgresSessionRepository
 from repositories.cache import CacheProtocol, RedisCache
 
-app = FastAPI(title="Symposium API")
+app = FastAPI(
+    title="Symposium API",
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc"
+)
 app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/api/v1/healthz")
